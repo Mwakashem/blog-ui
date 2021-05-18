66d56560717fe1916d7f6744ba55c1846adb4237
Mwakashem <shemmwaka1@gmail.com>
2021-05-18 11:09:03 +0300

    ui fix

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -7,7 +7,7 @@ export default function SinglePost() {
       <div className="singlePostWrapper">
         <img
           className="singlePostImg"
-          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          src="../20.jpg"
           alt=""
         />
         <h1 className="singlePostTitle">
@@ -22,7 +22,7 @@ export default function SinglePost() {
             Author:
             <b className="singlePostAuthor">
               <Link className="link" to="/posts?username=Safak">
-                Safak
+                Shem Mwaka
               </Link>
             </b>
           </span>
